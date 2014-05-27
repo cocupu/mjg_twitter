9d61ba561a229adf33bc735ea390585181937289
Matt Zumwalt <matt@curationexperts.com>
2014-05-26 21:04:33 -0500

    Publisher converts reports to valid json arrays

--- lib/report_publisher.rb
+++ lib/report_publisher.rb
@@ -4,6 +4,9 @@ class ReportPublisher
   attr_accessor :message
 
   def publish_reports(paths_to_reports)
+    paths_to_reports.each do |path_to_report|
+      convert_ndj_to_json(path_to_report)
+    end
     FileUtils.cp paths_to_reports, publish_to
     message = "Published #{paths_to_reports.length} reports to #{publish_to}"
   end
@@ -16,5 +19,12 @@ class ReportPublisher
       @publish_to = "./output/reports"
       FileUtils::mkdir_p @publish_to
     end
+  end
+
+  # Replaces newlines with commas.  Wraps the entire file's contents with square brackets.
+  def convert_ndj_to_json(path_to_report)
+    tmp_path =  path_to_report+".tmp"
+    %x(printf '%s' "[" |cat - #{path_to_report} | tr '\n' ',' | sed '$s/,$/]/' > #{tmp_path})
+    %x(mv #{tmp_path} #{path_to_report})
   end
 end
